24aa3a135c2239768399a40cf5b996f4c7ae77c9
Livius307 <livius.wiebe@gmail.com>
2025-12-23 13:36:32 +0100

    First custom items

--- src/main/java/net/Livius307/tutorialMod/TutorialMod.java
+++ src/main/java/net/Livius307/tutorialMod/TutorialMod.java
@@ -1,5 +1,7 @@
 package net.Livius307.tutorialMod;
 
+import net.Livius307.tutorialMod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -21,7 +23,7 @@ import net.neoforged.neoforge.event.server.ServerStartingEvent;
 @Mod(TutorialMod.MOD_ID)
 public class TutorialMod {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "liviusfirstmod";
+    public static final String MOD_ID = "tutorialmod";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
 
@@ -32,7 +34,7 @@ public class TutorialMod {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
-
+        ModItems.register(modEventBus);
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -46,7 +48,10 @@ public class TutorialMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BISMUTH);
+            event.accept(ModItems.RAW_BISMUTH);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
